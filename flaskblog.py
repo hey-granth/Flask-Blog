93a3ddfa2b725faa42c6e7ce60099daac7f7a442
Granth Agarwal <heygranth@gmail.com>
2024-10-26 15:31:03 +0530

    added forms.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-import lorem
+# import lorem
 
 
 app = Flask(__name__)
@@ -8,19 +8,19 @@ posts = [
     {
         'author': 'Granth Agarwal',
         'title': 'Blog Post 1',
-        'content': lorem.paragraph(),
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio lorem, ornare nec iaculis non, maximus eget augue. Phasellus elementum ultrices justo ut aliquet. Ut consequat tortor ac commodo porttitor. Aenean facilisis enim nibh, nec commodo risus interdum porta. Suspendisse diam urna, lobortis id ligula tristique, semper varius lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel diam risus. Vestibulum a quam eget mi vestibulum malesuada. Curabitur varius in enim eu varius. Integer vestibulum augue at enim efficitur auctor. Etiam non leo at nunc mattis consequat. ''',
         'date_posted': "October 23rd, 2024"
     },
     {
         'author': 'Ayush Jain',
         'title': 'Blog Post 2',
-        'content': lorem.paragraph(),
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio lorem, ornare nec iaculis non, maximus eget augue. Phasellus elementum ultrices justo ut aliquet. Ut consequat tortor ac commodo porttitor. Aenean facilisis enim nibh, nec commodo risus interdum porta. Suspendisse diam urna, lobortis id ligula tristique, semper varius lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel diam risus. Vestibulum a quam eget mi vestibulum malesuada. Curabitur varius in enim eu varius. Integer vestibulum augue at enim efficitur auctor. Etiam non leo at nunc mattis consequat. ''',
         'date_posted': "October 24th, 2024"
     }
 ]
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email')
+    password = PasswordField('Password')
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Sign Up')
