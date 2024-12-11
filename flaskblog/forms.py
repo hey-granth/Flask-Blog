321f322b82939ad67f6f2409f4ac3a6af76007e5
Granth Agarwal <heygranth@gmail.com>
2024-12-12 00:21:28 +0530

    improved registration system

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField(
@@ -13,6 +13,18 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    # Custom Validation (checks if the username already exists)
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("This username is already taken. Please choose a different one.")
+
+    # Custom Validation (checks if the email already exists)
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("This email is already taken. Please choose a different one.")
 
 
 class LoginForm(FlaskForm):
--- run.py
+++ run.py
@@ -3,6 +3,4 @@ from flaskblog import app
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
 # to run it in debug mode, use the command ```flask --app run.py --debug run```
