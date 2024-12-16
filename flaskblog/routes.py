83c45c593338f3af8abb1928300f41eb69f7181b
Granth Agarwal <heygranth@gmail.com>
2024-12-16 14:36:48 +0530

    added resizing for profile pictures

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,8 @@
+# imports
+
 from flask_login import login_user, current_user, logout_user, login_required
 import os, secrets
+from PIL import Image
 from flaskblog.models import User, Post
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -80,7 +83,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext     # This will save the picture with a random name
     picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
 
     return picture_fn
 
