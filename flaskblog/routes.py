a6fd1486b642865fb7a5892e08470da1cbee3caf
Granth Agarwal <heygranth@gmail.com>
2024-12-19 11:27:15 +0530

    added delete feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -128,6 +128,7 @@ def post(post_id):
 
 # Updating a post
 @app.route('/post/<int:post_id>/update')
+@login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
@@ -147,3 +148,17 @@ def update_post(post_id):
         form.title.data =  post.title
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+# Deleting a post
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Post has been deleted!', 'success')
+    return redirect(url_for('home'))
