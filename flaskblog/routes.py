47ee534c06c7d737dfde6b8496c77e028781da38
Granth Agarwal <heygranth@gmail.com>
2024-12-19 11:18:55 +0530

    added update feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,7 +5,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 import os, secrets
 from PIL import Image
 from flaskblog.models import User, Post
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog import app, db, bcrypt
 
@@ -124,3 +124,26 @@ def post(post_id):
     # get_or_404 will return the post with the given id, if it does not exist it will return a 404.
 
     return render_template('post.html', title=post.title, post=post)
+
+
+# Updating a post
+@app.route('/post/<int:post_id>/update')
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+        # Error 403 is a forbidden route, which means that the server understood the request, but the user is not allowed to access the route.
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        # we don't need to db.session.add(post) because the post is already in the database, we're just updating it
+
+        flash('Your Post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data =  post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
